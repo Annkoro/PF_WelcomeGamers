6d9e30805165f34d7a7513fd3856e5e4ad323fe4
Annkoro <0vk325551w33k8n@gmail.com>
2022-03-10 15:43:09 +0000

    [Add]post機能

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,2 +1,36 @@
 class Public::PostsController < ApplicationController
+
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to public_top_path
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+
+
+
+  private
+
+  def post_params
+    params.require(:posts).permit(:title, :caption)
+  end
+
 end
