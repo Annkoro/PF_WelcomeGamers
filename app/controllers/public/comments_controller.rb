e78b7106ff176b8df43de05d41e769ddbc1bdd54
Annkoro <0vk325551w33k8n@gmail.com>
2022-03-14 05:34:51 +0000

    [Update]コメント機能

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -9,8 +9,10 @@ class Public::CommentsController < ApplicationController
   end
 
   def destroy
-  Comment.find(params[:id]).destroy
-  redirect_to
+  # Comment.find(params[:id]).destroy
+  @comment = Comment.find(params[:id])
+  @comment.destroy
+  redirect_to post_path(params[:post_id])
   end
 
 
