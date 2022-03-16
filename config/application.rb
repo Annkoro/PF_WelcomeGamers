7bcbe55fe7c81a4257eb0b3f3c2e4d5c96d90634
Annkoro <0vk325551w33k8n@gmail.com>
2022-03-16 13:42:10 +0000

    [Update]レイアウト

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module PF
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Asia/Tokyo"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
