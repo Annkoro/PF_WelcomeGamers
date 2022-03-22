210db410242594a361e48c8d885a145e2ce7652a
Annkoro <0vk325551w33k8n@gmail.com>
2022-03-22 00:47:04 +0000

    [Update]post_view

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,8 @@ class Post < ApplicationRecord
   has_many :favorites, dependent: :destroy
 
   #バリデーションの設定
-  validates :title, presence: true
-  validates :caption, presence: true
+  validates :title, presence: true, length: {minimum: 5, maximum: 30}
+  validates :caption, presence: true, length: {minimum: 10, maximum: 400}
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
