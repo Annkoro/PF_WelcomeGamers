697665ae4d005f818c6b62b842df908add529958
Annkoro <0vk325551w33k8n@gmail.com>
2022-03-24 13:34:59 +0000

    [Update]お問い合わせ機能

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,2 @@
 class Contact < ApplicationRecord
-
-  validates :name, length: {minimum: 2, maximum: 20}
-  validates :email, length: {minimum: 2, maximum:30}
-  validates :subject, length: {minimum: 2, maximum: 30}
-  validates :message, length: {minimum: 2, maximum: 400}
-
 end
